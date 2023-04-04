3e58d998e8197bd2fe18adbe002384b3d97f9369
Marius Kaufman <110390261+Suiram1701@users.noreply.github.com>
2023-04-04 22:07:49 +0200

    Add Texture helper Add bitmap Convert to image

--- Pacman/Game.xaml.cs
+++ Pacman/Game.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Packman.Extension;
+using Packman.Style;
+using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,9 +22,7 @@ namespace Packman
     /// </summary>
     public partial class Game : Window
     {
-        public Game()
-        {
+        public Game() =>
             InitializeComponent();
-        }
     }
 }
