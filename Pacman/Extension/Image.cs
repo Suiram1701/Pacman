37cab7b273610b6205c331d70ecddb7a5246612f
Marius Kaufman <110390261+Suiram1701@users.noreply.github.com>
2023-04-04 22:07:49 +0200

    Add Texture helper Add bitmap Convert to image

---
+++ Pacman/Extension/Image.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace Packman.Extension
+{
+    internal static class Image
+    {
+        public static BitmapImage ToImage(this System.Drawing.Bitmap img)
+        {
+            MemoryStream MS = new MemoryStream();
+            img.Save(MS, System.Drawing.Imaging.ImageFormat.Bmp);
+
+            BitmapImage ix = new BitmapImage();
+            ix.BeginInit();
+            ix.CacheOption = BitmapCacheOption.OnLoad;
+            ix.StreamSource = MS;
+            ix.EndInit();
+            return ix;
+        }
+    }
+}
---
+++ Pacman/Style/TextureHelper.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace Packman.Style
+{
+    internal class TextureHelper : IEnumerable
+    {
+        private List<Bitmap> Bitmaps { get; }
+
+        public TextureHelper(Bitmap TextureCollection, int TextureHeight, int TextureWidht, int Rows, int Coloumns)
+        {
+            // Setup
+            Bitmaps = new List<Bitmap>();
+            Rectangle CopyRect = new Rectangle(0, 0, TextureWidht, TextureHeight);
+
+            // Get all bitmaps
+            for (int r = 0; r < Rows; r++)
+            {
+                for (int c = 0; c < Coloumns; c++)
+                {
+                    // Get bitmap
+                    Bitmaps.Add(TextureCollection.Clone(CopyRect, TextureCollection.PixelFormat));
+                    CopyRect.X += c * TextureWidht;
+                }
+                CopyRect.Y += r * TextureHeight;
+            }
+        }
+
+        public Bitmap this[int index] =>
+            Bitmaps[index];
+
+        public IEnumerator GetEnumerator() =>
+            Bitmaps.GetEnumerator();
+    }
+}
