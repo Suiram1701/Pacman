c12f27f9fc3a69d37ac8e3fc325255271b96f952
Marius Kaufman <110390261+Suiram1701@users.noreply.github.com>
2023-04-05 13:36:03 +0200

    Add row and colomn selection in texture helper

--- Pacman/Style/TextureHelper.cs
+++ Pacman/Style/TextureHelper.cs
@@ -12,17 +12,29 @@ namespace Packman.Style
     internal class TextureHelper : IEnumerable
     {
         private List<Bitmap> Bitmaps { get; }
+        private int Rows { get; }
+        private int Coloumns { get; }
 
-        public TextureHelper(Bitmap TextureCollection, int TextureHeight, int TextureWidht, int Rows, int Coloumns)
+        /// <summary>
+        /// Init the texture helper
+        /// </summary>
+        /// <param name="TextureCollection">Basic texture</param>
+        /// <param name="TextureHeight">Height of each texture</param>
+        /// <param name="TextureWidht">Widht of each texture</param>
+        /// <param name="Rows">All rows in the basic texture</param>
+        /// <param name="Colomns">All colomns in the basic texture</param>
+        public TextureHelper(Bitmap TextureCollection, int TextureHeight, int TextureWidht, int Rows, int Colomns)
         {
             // Setup
             Bitmaps = new List<Bitmap>();
+            this.Rows = Rows;
+            this.Coloumns = Coloumns;
             Rectangle CopyRect = new Rectangle(0, 0, TextureWidht, TextureHeight);
 
             // Get all bitmaps
             for (int r = 0; r < Rows; r++)
             {
-                for (int c = 0; c < Coloumns; c++)
+                for (int c = 0; c < Colomns; c++)
                 {
                     // Get bitmap
                     Bitmaps.Add(TextureCollection.Clone(CopyRect, TextureCollection.PixelFormat));
@@ -32,8 +44,38 @@ namespace Packman.Style
             }
         }
 
-        public Bitmap this[int index] =>
-            Bitmaps[index];
+        /// <summary>
+        /// Get a texture with their index of the texture
+        /// </summary>
+        /// <param name="index">Textures index</param>
+        /// <returns>The texture. When the index is the return value <see langword="null"/></returns>
+        public Bitmap this[int index]
+        {
+            get
+            {
+                if (index >= Bitmaps.Count())
+                    return null;
+
+                return Bitmaps[index];
+            }
+        }
+
+        /// <summary>
+        /// Get a texture with their row and colomn of the texture
+        /// </summary>
+        /// <param name="Row">Textures row</param>
+        /// <param name="Colomn">Textures colomn</param>
+        /// <returns>The texture. When the row or colomn isnt valid is the return value <see langword="null"/></returns>
+        public Bitmap this[int Row, int Colomn]
+        {
+            get
+            {
+                int index = Row * Coloumns + Colomn;
+                if (index >= Bitmaps.Count())
+                    return null;
+                return Bitmaps[index];
+            }
+        }
 
         public IEnumerator GetEnumerator() =>
             Bitmaps.GetEnumerator();
